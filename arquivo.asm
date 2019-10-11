fb020ee0d90b9752e80c4d122bdfe493cd4c4855
Felipe <ficechin@hotmail.com>
2019-10-10 22:20:40 -0300

    Programa para ler arquivo.

--- arquivo.asm
+++ arquivo.asm
@@ -1,6 +1,7 @@
 .data  
 fin: .asciiz "text.bin"      # filename for input
-buffer: .space 1024
+buffer: .space 2048
+palavra: .space 4
 
 .text
 #open a file for writing
@@ -15,13 +16,28 @@ move $s6, $v0      # save the file descriptor
 li   $v0, 14       # system call for read from file
 move $a0, $s6      # file descriptor 
 la   $a1, buffer   # address of buffer to which to read
-li   $a2, 1024     # hardcoded buffer length
+li   $a2, 2048     # hardcoded buffer length
 syscall            # read from file
 
-li $v0, 4
-la $a0, buffer
-syscall
+la $s0, buffer
+la $s1, palavra
+li $s2, 0
+proximo_comando:
+	li $s3, 0
+imprime_byte:
+	beq $s3, 4, proximo_comando
+	add $s0, $s0, $s2
+	lbu $s1, ($s0)
+	beq $s1, $s3, continua
+	li $v0, 34
+	add $a0, $s1, $zero
+	syscall
+	addi $s2, $s2, 1
+	addi $s3, $s3, 1
+	j imprime_byte
+	
 
+continua:
 # Close the file 
 li   $v0, 16       # system call for close file
 move $a0, $s6      # file descriptor to close
