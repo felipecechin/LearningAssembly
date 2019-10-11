7ed62ef9a8ab3168a3c136996b89e8271aa6251d
Felipe Cechin <ficechin@hotmail.com>
2019-10-10 22:57:19 -0300

    Lendo arquivo 2.

--- arquivo.asm
+++ arquivo.asm
@@ -1,7 +1,7 @@
 .data  
 fin: .asciiz "text.bin"      # filename for input
 buffer: .space 2048
-palavra: .space 4
+palavra: .space 2048
 
 .text
 #open a file for writing
@@ -32,39 +32,46 @@ syscall
 
 carrega_dados_arquivo:
     #prologo
-    addiu $sp, $sp, -20
+    addiu $sp, $sp, -24
     sw $s0, 0($sp)
     sw $ra, 4($sp)
     sw $s1, 8($sp)
     sw $s2, 12($sp)
     sw $s3, 16($sp)
+    sw $s4, 20($sp)
     
     #corpo 
     add $s0, $a0, $zero #$s0 dados do arquivo
     la $s1, palavra
     li $s2, 0
+    li $s3, 0
+    j imprime_byte
     proximo_comando:
+    	addi $s1, $s1, 4
         li $s3, 0
     imprime_byte:
         beq $s3, 4, verifica_palavra
+        j continua
         verifica_palavra:
-        	la $a0, palavra
+        	add $a0, $s1, $zero
         	jal verifica_palavra_nula
-        	beqz $v0, 
+        	beq $v0, 1, carrega_dados_arquivo_fim
+        	j proximo_comando
         continua:
-        	add $s1, $s1, $s3
+        	add $s4, $s1, $s3
         	add $s0, $s0, $s2
-        	lbu $s1, ($s0)
+        	lbu $s4, ($s0)
         	addi $s2, $s2, 1
         	addi $s3, $s3, 1
         	j imprime_byte
     
     #epilogo
     carrega_dados_arquivo_fim:
+    	lw $s4, 20($sp)
     	lw $s2, 12($sp) 
     	lw $s3, 16($sp)
     	lw $s1, 8($sp)
     	lw $s0, 0($sp)  
     	lw $ra, 4($sp)
-    	addiu $sp, $sp, 20
+    	addiu $sp, $sp, 24
     	jr $ra
