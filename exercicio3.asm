b0e413b260e5d41a81b309b762dd15444ccef9d0
Felipe Cechin <ficechin@hotmail.com>
2019-11-21 21:14:45 -0300

    Ajustes no exercício 3.

--- exercicio3.asm
+++ exercicio3.asm
@@ -1,5 +1,5 @@
 .data
-	msg_entreNumero:    .asciiz "Entre com um numero: "
+	msg_entreNumero: .asciiz "Entre com um numero: "
 	const2: .double 0.4  # constante 0.4, em ponto flutuante
   
     
@@ -17,21 +17,21 @@ fatorial:
 #prologo
             addiu $sp, $sp, -8      # ajusta a pilha para receber 2 itens
             sw    $ra, 4($sp)       # salva o endereço de retorno
-            sw    $a0, 0($sp)       # salva o argumento da função
+            sw    $a0, 0($sp)       # salva o argumento da funcao
 # corpo do procedimento
             bne   $zero, $a0, n_nao_igual_0 # se n!=0  calcule n*fatorial(n-1)
 n_igual_0:
             add   $v0, $zero, 1     # retorna 1 = 0!
-            j fatorial_epilogo      # epílogo  do procedimento
+            j fatorial_epilogo      # epilogo do procedimento
 n_nao_igual_0:
             # precisamos retornar n* fatorial(n-1)
-            # n está na pilha
+            # n esta na pilha
             # calculamos fatorial(n-1)
             addi  $a0, $a0, -1      # a0 <- n-1
             jal   fatorial          # chamamos fatorial(n-1)
             lw    $a0, 0($sp)       # a0 <- n, restauramos n
             mul   $v0, $a0, $v0     # v0 <- n*fatorial(n-1), v0 valor de retorno
-            lw    $ra, 4($sp)       # restaura o endereço de retorno
+            lw    $ra, 4($sp)       # restaura o endereco de retorno
 # epilogo
 fatorial_epilogo:
             add   $sp, $sp, 8       # restaura a pilha - eliminamos 2 itens
